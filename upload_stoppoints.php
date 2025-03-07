ce11ee21813f50e16f90cf91252ed1d38b2801e9
Christian Nolte <christian.nolte@nomadrail.com>
2025-03-07 11:24:09 +0100

    next try

--- upload_stoppoints.php
+++ upload_stoppoints.php
@@ -1,16 +1,16 @@
-<html>
-    <body>
-     <button onclick="history.back()">Go Back</button>
+
 <?php
 // Simple example for handling file upload in PHP
+echo '<html><body><button onclick="history.back()">Go Back</button>'
+
 if ($_FILES["file"]["error"] === UPLOAD_ERR_OK) {
     $tmp_name = $_FILES["file"]["tmp_name"];
+    echo $tmp_name
     $name = basename($_FILES["file"]["name"]);
     move_uploaded_file($tmp_name, __DIR__ . "/uploads/stop_points/uploaded_file.csv");
     echo "File successfully uploaded!";
 } else {
     echo "File upload error!";
 }
+echo '</body></html>'
 ?>
-</body>
-</html>
